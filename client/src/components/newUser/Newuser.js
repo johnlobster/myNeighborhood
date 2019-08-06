064554775db9e495707382bd97fa99eb81e6d014
eddsonblanco <eddsonrbr@gmail.com>
2019-08-06 02:36:06 -0700

    homePage add btn to login and signup/  Add Logo for the website/  Nav styles/ set up new routes and componets

---
+++ client/src/components/newUser/Newuser.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import './style.sass';
+// import { Link } from 'react-router-dom'
+
+
+function Newuser() {
+    return (
+        <div className="">
+            REGISTRATION FOR A NEW USER
+        </div>
+    );
+                  }
+                
+export default Newuser;
