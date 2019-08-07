052a247e96203a371400bb23ada45605c08d2189
John Webster <JohnLobster@comcast.net>
2019-08-07 09:29:09 -0700

    added default route '/' to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import Footer from "./components/footer/Footer";
 import Home from "./components/home/Home";
@@ -21,6 +21,7 @@ function App() {
             <Route path='/about'  component={About} />
             <Route path='/login' component={Login} />
             <Route path='/Newuser' component={Newuser} />
+            <Redirect to="/" /> 
           <Footer />
         </Switch>
         <Navbtn/>
