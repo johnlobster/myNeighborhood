5dff1834c01238a21d95ff1e2476fc195afe5306
Robert Ross <rob@campus-070-011.ucdavis.edu>
2019-08-08 19:21:20 -0700

    adding routes

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,10 @@ import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
 import Login from "./components/login/Login";
 import Newuser from './components/newUser/Newuser';
+import Recomendations from './components/recomendations/Recomendations';
+import Events from './components/events/Events';
+import Phonebook from './components/phonebook/Phonebook';
+import LocalInfo from './components/localinfo/LocalInfo';
 
 // themes imported here, but selector attaches them to <body>
 import "./styles/themes.scss";
@@ -17,9 +21,13 @@ function App() {
           <Nav/>
           <Switch>
             <Route exact path='/'  component={Home} />
-            <Route path='/about'  component={About} />
-            <Route path='/login' component={Login} />
+            <Route path='/About'  component={About} />
+            <Route path='/Login' component={Login} />
             <Route path='/Newuser' component={Newuser} />
+            <Route path='/Recomendations' component={Recomendations} />
+            <Route path='/Events' component={Events} />
+            <Route path='/Phonebook' component={Phonebook} />
+            <Route path='/LocalInfo' component={LocalInfo} />
           <Footer />
         </Switch>
     </Router>
