9d9abbc1120b00fc96a786516977419be41be8f5
eddsonblanco <eddsonrbr@gmail.com>
2019-08-13 21:02:27 -0700

    registration style

--- client/src/components/navbtn/Navbtn.js
+++ client/src/components/navbtn/Navbtn.js
@@ -1,6 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-has-content */
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import './styles.sass';
 import { Link } from 'react-router-dom';
@@ -11,11 +8,11 @@ function Navbtn() {
   
   
   return (
-      <div className="row d-flex flex-row-reverse">
+      <div id="wrapper-btnnav" className="row d-flex flex-row-reverse">
       <div id="nav" className="col-3 fixed-bottom ">
           <li className="nav-item dropdown p-2 ">
             <Link className="nav-link" to="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i className="fa fa-bars" aria-hidden="true"></i>
+            <i id="bars-menu" className="fa fa-bars" aria-hidden="true"></i>
             </Link>
             <div className="dropdown-menu" aria-labelledby="navbarDropdown">
               <Link className="dropdown-item text-right" to="/Services">Services<i className="fa fa-tools"></i></Link>
