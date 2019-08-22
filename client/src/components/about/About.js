10752f328db00b9396280fcd8c4b14d60bdf9c64
eddsonblanco <eddsonrbr@gmail.com>
2019-08-21 23:05:08 -0700

    about us eddson done

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import './style.sass';
+import eddson from '../../images/eddson2.jpg';
+import diego from '../../images/cat.jpg';
+import john from '../../images/john.jpg';
 
 
 
@@ -9,20 +12,20 @@ function About() {
         <div style={{ display: 'flex', flexDirection:'column',  }}>
             <h1 style={{textAlign:'center', marginTop:'3rem', marginBottom:'3rem'}}> Our Team</h1>
         <Card  style={{ width: '20rem', margin:'auto',marginBottom:'1rem' }}>
-            <Card.Img variant="top" src="https://images.unsplash.com/photo-1559548331-f9cb98001426?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=280&q=80" />
+            <Card.Img variant="top" src={eddson} />
             <Card.Body>
                 <Card.Title>Eddson Blanco</Card.Title>
                 <Card.Text>
-                I became eager to learn and took it into my own hands to teach myself, creating my first fully functional website for a local restaurant, utilizing html and css. From then on, I decided to pursue a career as a web developer and have been working endlessly towards expanding my skill set. 
+                I'm from Caracas, Venezuela and I immigrated to the US 4 years ago. I consider myself a creative, driven and very social person.<p>In my spare time, you can catch me on the dance floor dancing Salsa, traveling or playing video games</p>  
                 </Card.Text>
                 <div style={{display:'flex',  }}>
-                <a style={{fontSize:'2rem',color:'rgba(133, 183, 59, 1)', marginRight:'1rem'  }} href="https://linkedin.com/in/eddson-blanco-94a919168/"><i className="fab fa-linkedin"></i> </a>
-                <a style={{fontSize:'2rem',color:'rgba(133, 183, 59, 1)', marginRight:'1rem'  }} href="https://github.com/Eddsonblanco"><i className="fab fa-github-square"></i></a>
+                <a target="_blank" style={{fontSize:'2rem',color:'rgba(133, 183, 59, 1)', marginRight:'1rem'  }} href="https://linkedin.com/in/eddson-blanco-94a919168/"><i className="fab fa-linkedin"></i> </a>
+                <a target="_blank" style={{fontSize:'2rem',color:'rgba(133, 183, 59, 1)', marginRight:'1rem'  }} href="https://github.com/Eddsonblanco"><i className="fab fa-github-square"></i></a>
                 </div>
             </Card.Body>
         </Card>
         <Card  style={{ width: '20rem', margin:'auto',marginBottom:'1rem' }}>
-            <Card.Img variant="top" src="https://images.unsplash.com/photo-1559548331-f9cb98001426?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=280&q=80" />
+            <Card.Img variant="top" src={john} />
             <Card.Body>
                 <Card.Title>John Lobster</Card.Title>
                 <Card.Text>
@@ -30,22 +33,22 @@ function About() {
                     the card's content.
                 </Card.Text>
                 <div style={{display:'flex',  }}>
-                <a style={{fontSize:'2rem',color:'rgba(133, 183, 59, 1)', marginRight:'1rem'  }} href="https://linkedin.com/in/eddson-blanco-94a919168/"><i className="fab fa-linkedin"></i> </a>
-                <a style={{fontSize:'2rem',color:'rgba(133, 183, 59, 1)', marginRight:'1rem'  }} href="https://github.com/Eddsonblanco"><i className="fab fa-github-square"></i></a>
+                <a target="_blank" style={{fontSize:'2rem',color:'rgba(133, 183, 59, 1)', marginRight:'1rem'  }} href="https://linkedin.com/in/eddson-blanco-94a919168/"><i className="fab fa-linkedin"></i> </a>
+                <a target="_blank" style={{fontSize:'2rem',color:'rgba(133, 183, 59, 1)', marginRight:'1rem'  }} href="https://github.com/Eddsonblanco"><i className="fab fa-github-square"></i></a>
                 </div>
             </Card.Body>
         </Card>
         
         <Card  style={{ width: '20rem', margin:'auto',marginBottom:'1rem' }}>
-            <Card.Img variant="top" src="https://images.unsplash.com/photo-1559548331-f9cb98001426?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=280&q=80" />
+            <Card.Img variant="top" src={diego} />
             <Card.Body>
                 <Card.Title>Diego Solorio</Card.Title>
                 <Card.Text>
                 After years of doubt, I finally took a shot at coding. It's been a tremendous struggle and journey, but I wouldn't change it one bit.
                 </Card.Text>
                 <div style={{display:'flex',  }}>
-                <a style={{fontSize:'2rem',color:'rgba(133, 183, 59, 1)', marginRight:'1rem'  }} href="https://www.linkedin.com/in/diego-solorio-236374177/"><i className="fab fa-linkedin"></i> </a>
-                <a style={{fontSize:'2rem',color:'rgba(133, 183, 59, 1)', marginRight:'1rem'  }} href="https://github.com/diesol49"><i className="fab fa-github-square"></i></a>
+                <a target="_blank" style={{fontSize:'2rem',color:'rgba(133, 183, 59, 1)', marginRight:'1rem'  }} href="https://www.linkedin.com/in/diego-solorio-236374177/"><i className="fab fa-linkedin"></i> </a>
+                <a target="_blank" style={{fontSize:'2rem',color:'rgba(133, 183, 59, 1)', marginRight:'1rem'  }} href="https://github.com/diesol49"><i className="fab fa-github-square"></i></a>
                 </div>
             </Card.Body>
         </Card>
@@ -59,8 +62,8 @@ function About() {
                     the card's content.
                 </Card.Text>
                 <div style={{display:'flex',  }}>
-                <a style={{fontSize:'2rem',color:'rgba(133, 183, 59, 1)', marginRight:'1rem'  }} href="https://www.linkedin.com/in/thinksmartrob/"><i className="fab fa-linkedin"></i> </a>
-                <a style={{fontSize:'2rem',color:'rgba(133, 183, 59, 1)', marginRight:'1rem'  }} href="https://github.com/Sidetrack-CA"><i className="fab fa-github-square"></i></a>
+                <a target="_blank" style={{fontSize:'2rem',color:'rgba(133, 183, 59, 1)', marginRight:'1rem'  }} href="https://www.linkedin.com/in/thinksmartrob/"><i className="fab fa-linkedin"></i> </a>
+                <a target="_blank" style={{fontSize:'2rem',color:'rgba(133, 183, 59, 1)', marginRight:'1rem'  }} href="https://github.com/Sidetrack-CA"><i className="fab fa-github-square"></i></a>
                 </div>
             </Card.Body>
         
